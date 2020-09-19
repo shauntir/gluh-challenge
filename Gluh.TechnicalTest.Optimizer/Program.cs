c22b0070c5bb0d4c24def508395201dc863af39d
Shaun Tirubeni <shauntir@gmail.com>
2020-09-19 12:16:07 +1000

    Initial commit with project structure

---
+++ Gluh.TechnicalTest.Optimizer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Gluh.TechnicalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var purchaseRequirements = new TestData().Create();
+            var purchaseOptimizer = new PurchaseOptimizer();
+
+            purchaseOptimizer.Optimize(purchaseRequirements);
+
+            Console.WriteLine("App");
+            Console.Read();
+        }
+    }
+}
---
+++ Gluh.TechnicalTest.Optimizer/PurchaseOptimizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gluh.TechnicalTest.Models;
+using Gluh.TechnicalTest.Database;
+
+namespace Gluh.TechnicalTest
+{
+    public class PurchaseOptimizer
+    {
+        /// <summary>
+        /// Calculates the optimal set of supplier to purchase products from.
+        /// ### Complete this method
+        /// </summary>
+        public void Optimize(List<PurchaseRequirement> purchaseRequirements)
+        {
+            
+        }
+    }
+}
