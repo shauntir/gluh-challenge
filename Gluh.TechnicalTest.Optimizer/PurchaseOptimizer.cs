578777dbc1622a31e55d91d37a5611cf7004cadb
Shaun Tirubeni <shauntir@gmail.com>
2020-09-20 19:22:05 +1000

    Print messages nicely

--- Gluh.TechnicalTest.Optimizer/PurchaseOptimizer.cs
+++ Gluh.TechnicalTest.Optimizer/PurchaseOptimizer.cs
@@ -20,6 +20,8 @@ namespace Gluh.TechnicalTest
         /// </summary>
         public void Optimize(List<PurchaseRequirement> purchaseRequirements)
         {
+            PrintPurchaseRequirements(purchaseRequirements);
+
             var purchaseOrderItems = _allFulfillmentServices.SelectMany(x => x.GetPurchaseOrderItems(purchaseRequirements));
             var supplierPurchaserOrders = purchaseOrderItems
                 .GroupBy(x => x.SupplierToFulfull)
@@ -30,14 +32,32 @@ namespace Gluh.TechnicalTest
                 })
                 .ToList();
 
+            PrintPurchaseOrders(supplierPurchaserOrders);
+        }
+
+        private void PrintPurchaseRequirements(List<PurchaseRequirement> purchaseRequirements)
+        {
+            Console.WriteLine($"~~~~~~~~~~~~~~~~ Purchase Requirements Section ~~~~~~~~~~~~~~~~");
+            foreach (var item in purchaseRequirements)
+            {
+                Console.WriteLine($"------------- Purchase Requirement -------------");
+                Console.WriteLine($"{item.Product.Name} needs to be fulfilled at a quantity of {item.Quantity}");
+                Console.WriteLine($"-------------------------------------------------------");
+                Console.WriteLine();
+            }
+        }
+
+        private void PrintPurchaseOrders(List<PurchaseOrder> supplierPurchaserOrders)
+        {
+            Console.WriteLine($"~~~~~~~~~~~~~~~~ Purchase Order Section ~~~~~~~~~~~~~~~~");
             foreach (var purchaseOrder in supplierPurchaserOrders)
             {
-                Console.WriteLine($"------------- Purchase order -------------");
+                Console.WriteLine($"------------- Purchase order for Supplier -------------");
                 foreach (var item in purchaseOrder.PurchasOrderItems)
                 {
                     Console.WriteLine(item);
                 }
-                Console.WriteLine($"------------------------------------------");
+                Console.WriteLine($"-------------------------------------------------------");
                 Console.WriteLine();
             }
         }
